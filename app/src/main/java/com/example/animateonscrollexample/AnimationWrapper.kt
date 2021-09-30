57f94e7563408c8100948889ee770f70d899f410
정민지 <s2018s38@e-mirim.hs.kr>
2021-10-01 01:25:26 +0900

    스크롤 인터렉션 그룹 지정을 위한 Wrapper 클래스 이름 및 옵션 수정
    - CustomViewHolder -> AnimationWrapper
    - 처음 시작되는 alpha, y값을 외부에서 지정할 수 있도록 수정

--- app/src/main/java/com/example/animateonscrollexample/AnimationWrapper.kt
+++ app/src/main/java/com/example/animateonscrollexample/AnimationWrapper.kt
@@ -2,27 +2,33 @@ package com.example.animateonscrollexample
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.Resources
 import android.util.AttributeSet
 import android.view.ViewPropertyAnimator
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.widget.NestedScrollView
 
-class CustomViewHolder @JvmOverloads constructor(
+class AnimationWrapper @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private val delay = context.obtainStyledAttributes(attrs, R.styleable.CustomViewHolder)
+    var customY: Float = 20.dp
+    var customAlpha: Float = 0.2f
+
+    @SuppressLint("CustomViewStyleable")
+    var delay = context.obtainStyledAttributes(attrs, R.styleable.CustomViewHolder)
         .getInt(R.styleable.CustomViewHolder_delayAnimation, 0).toLong()
+
     private var isVisible = false // 현재 보여지고 있는지 (두 번 애니메이션 시키지 않기 위해서)
     private val location = IntArray(2)
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
 
-        this.y = 900f
-        this.alpha = 0f
+        this.y = customY
+        this.alpha = customAlpha
 
         (parent.parent as NestedScrollView).viewTreeObserver.addOnGlobalLayoutListener {
             getLocationOnScreen(location)
@@ -62,5 +68,8 @@ class CustomViewHolder @JvmOverloads constructor(
     companion object {
         private const val ANIMATION_GUIDE_LINE: Int = 500
         private const val ANIMATION_DURATION: Long = 1000
+
+        val Int.dp: Float
+            get() = (this * Resources.getSystem().displayMetrics.density + 0.5f)
     }
 }
